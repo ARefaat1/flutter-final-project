a072289100ade7f3ea407023fe9011129d164d9d
Ahmed Refaat <167882120+ARefaat1@users.noreply.github.com>
2025-08-25 23:48:33 +0300

    favorites merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_final_project/features/Home/presentation/page/home_screen.dart';
-import 'package:flutter_final_project/features/favorites/presentation/page/favorites_screen.dart';
 import 'package:flutter_final_project/features/splach_screen/presentation/page/splach_screen.dart';
 
 void main() {
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home:  FavoritesScreen(),
+      home: const SplashScreen(),
     );
   }
 }
